38f96e945738a1d866096ca260f205f992e5e80f
DLavin23 <daniel.e.lavin@gmail.com>
2012-06-01 15:32:49 -0500

    added validations to story model

--- app/models/story.rb
+++ app/models/story.rb
@@ -2,4 +2,5 @@ class Story < ActiveRecord::Base
   attr_accessible :description, :frequency, :name, :priority
   belongs_to :user
   belongs_to :backlog
+  validates :name, :description, :priority, :presence => true
 end
